00291d07eaf1bae0713d3ad4da2ee783650f6f1e
Jasper Van Damme <M999JAV@MUTWORLD.BE>
2019-07-17 16:07:08 +0200

    fixed some errors in PS script, added an API status monitor in case API is not reachable

--- UptimeRobot/Health Model/Monitors/Scripts/GetMonitorsStatus.ps1
+++ UptimeRobot/Health Model/Monitors/Scripts/GetMonitorsStatus.ps1
@@ -4,6 +4,11 @@
 $api = new-object -comObject 'MOM.ScriptAPI'
 $bag = $api.CreatePropertyBag() 
 #check Uptime Robot status page
+
+#Accept TLS1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
 $Header = @{
     "Content-Type" = "application/x-www-form-urlencoded"
     "Cache-Control" = "no-cache"
@@ -18,13 +23,21 @@ $body = @{
 
 $URL = 'https://api.uptimerobot.com/v2/getMonitors'
 
+Try{
 
+	$webMonitor = (Invoke-RestMethod -Method POST -uri $URL -Headers  $header -Body $body).monitors | where id -EQ $id
 
-$webMonitor = (Invoke-RestMethod -Method POST -uri $URL -Headers  $header -Body $body).monitors | where id -EQ $id
+}
 
+Catch
+
+{
+	$api.LogScriptEvent($ScriptName, 10014, 2, "Failed to query the API, the error was: $Error")
+}
 #Declare variables containing status
 [int]$strStatus = $WebMonitor.status
 
 #add the values to the bag
 $bag.AddValue('Status', $strStatus)
 $bag 
+
---
+++ UptimeRobot/Health Model/Monitors/Scripts/GetUptimeRobotAPIStatus.ps1
@@ -0,0 +1,44 @@
+﻿param($APIkey)
+
+#Create monitor property bag
+$api = new-object -comObject 'MOM.ScriptAPI'
+$bag = $api.CreatePropertyBag()
+
+#Accept TLS1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#check Uptime Robot status page
+$Header = @{
+    "Content-Type" = "application/x-www-form-urlencoded"
+    "Cache-Control" = "no-cache"
+    
+}
+
+$body = @{
+	"api_key" = $APIkey
+	"time_zone" = "1"
+	"format" = "json"
+}
+
+$URL = 'https://api.uptimerobot.com/v2/getMonitors'
+
+Try{
+
+	$webMonitor = Invoke-RestMethod -Method POST -uri $URL -Headers  $header -Body $body
+	If($webMonitor)
+		{
+			$bag.AddValue('Status',"NOK")
+			$bag.AddValue('URL',$URL)
+		}
+	Else
+		{
+			$bag.AddValue('Status',"OK")
+			$bag.AddValue('URL',$URL)
+		}
+}
+Catch
+{
+	$api.LogScriptEvent($ScriptName, 10016, 2, "Failed to query the API, the error was: $Error")
+	$bag.AddValue('Error',$Error)
+}
+
